2f8d0f1e6ac0a9522d1435392eb10b8f4a66daeb
gigas002 <gigas002@pm.me>
2024-11-24 12:14:46 +0900

    make image argument positional

--- src/args.rs
+++ src/args.rs
@@ -3,8 +3,8 @@ use std::path::PathBuf;
 
 #[derive(Clone, Debug, Parser, Default)]
 pub struct Args {
-    #[arg(short, long)]
     pub img: PathBuf,
+
     #[arg(short, long)]
     pub config: Option<PathBuf>,
 }
